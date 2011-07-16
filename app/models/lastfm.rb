499869e229ea4f2bb5385cbd2c6ac02f5805cf34
Jake Dahn <admin@jakedahn.com>
2011-07-16 01:12:49 -0700

    adding lastfm model

--- app/models/lastfm.rb
+++ app/models/lastfm.rb
@@ -1,2 +1,10 @@
 class Lastfm < ActiveRecord::Base
+  has_one :post
+  after_create :create_post
+  validates_uniqueness_of :t_id
+
+private
+  def create_post
+    Post.create(:post_type => "twitter", :twitter_id => self.id, :created_at => self.created_at)
+  end
 end
