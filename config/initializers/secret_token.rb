2fe13cddb2e43ac924317209f80545100db5781d
Jake Dahn <admin@jakedahn.com>
2011-07-15 20:21:32 -0700

    initial commit from rails new lifestreamed -m http://railswizard.org/17de5cb9359b10e693f5.rb -T -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifestreamed::Application.config.secret_token = '18c3372253b0e730dd89b63690c1b3a885b7ddb6138836b3f290d5366feddfcda44ff2ea79fc8cd6c6ce1f475f65f8dd30983dc5d3fd5eeeac74781dafcd4e4e'
