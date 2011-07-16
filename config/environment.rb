fb16102a374cedfb165ee339e95d86dfcbb22364
Jake Dahn <admin@jakedahn.com>
2011-07-16 01:10:32 -0700

    fetching and parsing of songs from lastfm now works via client

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lifestreamed::Application.initialize!
+
+# load api keys from config file
+API_KEYS = YAML.load_file("#{RAILS_ROOT}/config/api_keys.yml")
---
+++ lib/songs.rb
@@ -0,0 +1,43 @@
+class Songs
+  include HTTParty
+
+  base_uri 'http://ws.audioscrobbler.com/2.0'
+  disable_rails_query_string_format
+
+  def fetch(user, method='user.getrecenttracks', format='json', limit=50)
+    self.class.get("/", 
+      :query => {
+        :method => method,
+        :format => format,
+        :api_key => API_KEYS['lastfm']['key'],
+        :user => user,
+        :limit => limit
+    })['recenttracks']['track']
+  end
+
+  def parse_song(song)
+    parsed_song = {
+      :time_id => song['date']['uts'],
+      :artist => song['artist']['#text'],
+      :album => song['album']['#text'],
+      :track => song['name'],
+      :url => song['url'],
+      :created_at => Time.at(song['date']['uts'].to_i)
+    }
+    return parsed_song
+  end
+
+  def fetch_formatted(user, method='user.getrecenttracks', format='json', limit=50)
+    raw_songs = fetch(user, method, format, limit)
+    raw_songs.pop
+    raw_songs.shift
+
+    parsed_songs = []
+
+    for song in raw_songs
+      parsed_songs.push(self.parse_song(song))
+    end
+
+    return parsed_songs
+  end
+end
