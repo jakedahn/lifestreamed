aa44ed486482dbf2e893cb9e00f1c50da404a445
Jake Dahn <admin@jakedahn.com>
2011-07-16 05:37:44 -0700

    moving oauth link info to the proper controller

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,5 +1,10 @@
 class OauthController < ApplicationController
   def instagram
+    client_id = ENV['instagram_client_id'].blank? ? API_KEYS['instagram']['client_id'] : ENV['instagram_client_id']
+    redirect_url = ENV['instagram_redirect_url'].blank? ? API_KEYS['instagram']['redirect_url'] : ENV['instagram_redirect_url']
+    
+    @instagram_auth_url = "https://instagram.com/oauth/authorize/?client_id=#{client_id}&redirect_uri=#{redirect_url}&response_type=token"
+    
     if params[:access_token]
       Service.create(:name => 'instagram', :username => params[:access_token])  
       redirect_to "/"
--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -1,12 +1,5 @@
 class SetupController < ApplicationController
   def install
-    
-    client_id = ENV['instagram_client_id'].blank? ? API_KEYS['instagram']['client_id'] : ENV['instagram_client_id']
-    redirect_url = ENV['instagram_redirect_url'].blank? ? API_KEYS['instagram']['redirect_url'] : ENV['instagram_redirect_url']
-    
-    @instagram_auth_url = "https://instagram.com/oauth/authorize/?client_id=#{client_id}&redirect_uri=#{redirect_url}&response_type=token"
-    
-    
   end
 
   def services
