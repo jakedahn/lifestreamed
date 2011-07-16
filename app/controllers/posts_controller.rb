8dcbfdcb9517d792f32ab3586f3fc67665316e33
Jake Dahn <admin@jakedahn.com>
2011-07-16 05:18:11 -0700

    added songs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @tweets = Post.where("post_type = 'twitter'")
-    @songs = Post.where("post_type = 'lastfm'")
-    @photos = Post.where("post_type = 'instagram'")
+    @tweets = Post.where("post_type = 'twitter'").limit(25)
+    @songs = Post.where("post_type = 'lastfm'").limit(25)
+    @photos = Post.where("post_type = 'instagram'").limit(10)
   end
 
   def show
